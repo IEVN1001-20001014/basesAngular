691becbc9d278924ba30cda5b84bcba988db625f
Dulce Carmona <20001014@alumnos.utleon.edu.mx>
2024-09-19 21:01:57 -0600

    2do commit Jueves botón

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+  imageWidth:number=300;
+  imageMargin:number=3;
+  muestraImg:boolean=true;
+  mostrarImagen():void{
+    this.muestraImg=!this.muestraImg
+  }
+
+  productos:any[]=[
+    {
+      "ProductoId":1,
+      "Modelo":"Sentra",
+      "Descripcion":"2 puertas",
+      "Year":"febrero 2 2020",
+      "Precio":20000,
+      "Color":"Azul",
+      "Marca":"AUDI",
+      "ImageUrl":"https://www.nissan.com.mx/content/dam/Nissan/mexico/assets/sentra/my24/vap/nissan-2024-sentra_sense_tm-d.jpg.ximg.l_12_m.smart.jpg"
+    },
+    {
+      "ProductoId":2,
+      "Modelo":"A4",
+      "Descripcion":"4 puertas",
+      "Year":"marzo 12 2021",
+      "Precio":120000,
+      "Color":"Blanco",
+      "Marca":"NISSAN",
+      "ImageUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVekYGBMRWkH4-dsoCa5rAoHQwiFoIm5VD8w&s"
+    },
+    {
+      "ProductoId":3,
+      "Modelo":"Rio",
+      "Descripcion":"2 puertas",
+      "Year":"febrero 2 2005",
+      "Precio":70000,
+      "Color":"Rojo",
+      "Marca":"BMW",
+      "ImageUrl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ63xVtaNERfxeg6Oi5NipBB5HCg90tnrzYHw&s"
+    },
+  ]
 }
